e9d5e0f88bbf28904fdbf2f46589cc431048b60f
JustCallMeSimon <no>
2022-07-08 17:53:21 +0200

    Mostly working. TODO: -Preview -Copy Image (the image itself)

--- Crawler.py
+++ Crawler.py
@@ -7,17 +7,22 @@ import random
 import urllib.request
 import requests
 
-
 class LSCrawler:
 	def __init__(self):
-		self.expectedImageAmount = 3
-		self.realImageIndex = 0
+		debugPrint("Starting...")
 		self.chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+		self.folderSetup()
+		debugPrint("Startup complete.")
+
+####################################################
+#         Functions below are for the gui          #
+####################################################
+
+		#GUI
 		dpg.create_context()
 		dpg.create_viewport(title='LS-Crawler', width=562, height=250)
 		dpg.setup_dearpygui()
 		dpg.show_viewport()
-		self.setup()
 		self.initMainWindow()
 		self.setWindowAsPrimary()
 		self.run()
@@ -25,16 +30,16 @@ class LSCrawler:
 	def initMainWindow(self):
 		with dpg.texture_registry():
 			width, height, _, data = dpg.load_image("./assets/placeholder.png")
-			texture_id = dpg.add_dynamic_texture(width, height, data, tag="ImagePreview")
+			self.texture_id = dpg.add_dynamic_texture(width, height, data, tag="ImagePreview")
 		with dpg.window(tag="Window"):
 			#Image Button
-			dpg.add_image_button(texture_tag=texture_id,width=200,height=175,pos=(12,12))
+			dpg.add_image_button(texture_tag=self.texture_id,width=200,height=175,pos=(12,12))
 			#Next Image Button
 			dpg.add_button(label="Next Image",callback=self.nextImage, pos=(232,14), width=300, height=50)
 			#Copy Image Button
-			dpg.add_button(label="Copy Image",callback=self.placeholder, pos=(232,76), width=300, height=50)
+			dpg.add_button(label="Copy Image",callback=self.copyImage, pos=(232,76), width=300, height=50)
 			#Copy Image URL Button
-			dpg.add_button(label="Copy Image URL",callback=self.placeholder, pos=(232,138), width=300, height=51)
+			dpg.add_button(label="Copy Image URL",callback=self.copyImageURL, pos=(232,138), width=300, height=51)
 
 	def setWindowAsPrimary(self):
 		dpg.set_primary_window("Window", True)
@@ -44,51 +49,75 @@ class LSCrawler:
 		dpg.destroy_context()
 
 ####################################################
-#     Functions below are for the actual work      #
+#     Functions below are for the buttons          #
 ####################################################
 
 	def nextImage(self):
-		code = self.genURLCode()
-		pathToImage = self.downloadImage(code)
-		print(pathToImage)
+		self.ImageData = self.downloadImage(self.genURLCode())
+		if not self.ImageData:
+			debugPrint("Failed to download image. Trying again.")
+			self.nextImage()
+		else:
+			self.setImageToPreview(self.ImageData["ImagePath"])
+
+	def copyImage(self):
+		try:
+			path = os.path.expandvars(self.ImageData["ImagePath"])
+
+
+		except KeyError:
+			os.system("echo " + "I tried to copy a Image before i generated a Image. Man i am dumb lol" + " | clip")
+
+	def copyImageURL(self):
+		try:
+			os.system("echo " + self.ImageData["ImageURL"] + " | clip")
+		except KeyError:
+			os.system("echo " + "I tried to copy a Image Url before i generated a Image. Man i am dumb lol" + " | clip")
+
+	def setImageToPreview(self, path):
+		with dpg.texture_registry():
+			_, _, _, data = dpg.load_image("./assets/placeholder.png")
+			dpg.set_value(self.texture_id, data=data)
 
 ####################################################
-#      Functions below are helper functions        #
+#      Functions below do the actual work          #
 ####################################################
 
 	def downloadImage(self, code):
-		webpage = BeautifulSoup(urllib.request.urlopen(urllib.request.Request(f"https://prnt.sc/{code}",headers={'User-Agent': 'Mozilla/5.0'})).read().decode('ISO-8859-1'), 'html.parser')
+		debugPrint("#####Start of Download Thread#####")
+		debugPrint(f"Trying to download image from https://prnt.sc/{code}")
+		try:
+			webpage = BeautifulSoup(urllib.request.urlopen(urllib.request.Request(f"https://prnt.sc/{code}",headers={'User-Agent': 'Mozilla/5.0'}),timeout=20).read().decode('ISO-8859-1'), 'html.parser')
+		except:
+			debugPrint("Timeout reached. (20 Sec)")
+			debugPrint("######End of Download Thread######")
+			return False
 		imageURLs = webpage.find_all('img')
-		if len(imageURLs) != self.expectedImageAmount:
-			self.nextImage()
-			return
+		debugPrint(f"Found {len(imageURLs)} images.")
+		for imageURL in imageURLs:
+			debugPrint(f"{imageURL['src']}")
+		debugPrint(f"Choosing {imageURLs[0]['src']}")
+		if not str(imageURLs[0]['src']).startswith("https://"):
+			debugPrint("Invalid image URL.")
+			debugPrint("######End of Download Thread######")
+			return False
+		if not os.path.isfile(os.path.expandvars(f"%appdata%/LSCrawler/Screenshots/{code}.png")):
+			with requests.get(str(imageURLs[0]['src']),headers={'User-Agent': 'Mozilla/5.0'}, stream=True) as url:
+				#test if the request returned a 404 response
+				if url.status_code == 404:
+					debugPrint("Image not found.")
+					return False
+				with open(str(os.path.expandvars(f"%appdata%/LSCrawler/Screenshots/{code}.png")), "wb") as file:
+					shutil.copyfileobj(url.raw, file)
 		else:
-			addon= ""
-			if not os.path.isfile(os.path.expandvars(f"%appdata%/LSCrawler/Screenshots/{code}.png")):
-				if not str(imageURLs[self.realImageIndex]['src']).startswith("http"):
-					addon = "https:"
-				with requests.get(addon + str(imageURLs[self.realImageIndex]['src']),headers={'User-Agent': 'Mozilla/5.0'}, stream=True) as url:
-					with open(str(os.path.expandvars(f"%appdata%/LSCrawler/Screenshots/{code}.png")), "wb") as file:
-						shutil.copyfileobj(url.raw, file)
-			print(imageURLs[self.realImageIndex]['src'])
-			return os.path.expandvars(f"%appdata%/LSCrawler/Screenshots/{code}.png")
+			debugPrint(f"Image already exists.")
+			debugPrint("######End of Download Thread######")
+			return False
+		debugPrint("######End of Download Thread######")
+		results = {"ImagePath":f"%appdata%/LSCrawler/Screenshots/{code}.png","ImageURL":f"https://prnt.sc/{code}"}
+		return results
 
-		# def downloadImage(self,filename = "idk", path = "./Screenshots/"):
-		# if not os.path.isfile(f"{path}{filename}.png"):
-		# 	try:
-		# 		#* Using a User Agent to not be *sus*
-		# 		with requests.get(self.imageURL,headers={'User-Agent': 'Mozilla/5.0'}, stream=True) as url:
-		# 			with open(path + filename + ".png", "wb") as file:
-		# 				shutil.copyfileobj(url.raw, file)
-		# 		self.path = path + filename
-		# 	except MissingSchema:
-		# 		print("shit")
-		# 	except FileNotFoundError:
-		# 		os.makedirs(path)
-		# 		self.downloadImage(filename = filename)
-
-
-	def setup(self):
+	def folderSetup(self):
 		if not os.path.exists(os.path.expandvars("%appdata%/LSCrawler/Screenshots")):
 			os.makedirs(os.path.expandvars("%appdata%/LSCrawler/Screenshots"))
 
@@ -101,6 +130,14 @@ class LSCrawler:
 			code += self.chars[random.randint(0,len(self.chars)-1)]
 		return code
 
+####################################################
+#     Functions below are helper functions         #
+####################################################
+
+debug = True
+def debugPrint(message):
+	if debug:
+		print(f"[LS]> {message}")
 
 
 if __name__ == "__main__":
